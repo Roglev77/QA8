8dde3a576c59f87e8b614da0101e2f3002002a73
Your Name <you@example.com>
2024-03-24 15:22:45 +0200

    First commit

--- HW1.java
+++ HW1.java
@@ -1,2 +1,12 @@
 public class HW1 {
+    public static void main(String[] args){
+        int a = 7;
+        int b = 9;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a" + " = " + a);
+        System.out.println("b" + " = " + b);
+    }
+
 }
